a527c22537ce65c3ca01ecd2edd0f1d6f2d656d3
alexraduca01 <alex.raduca01@icloud.com>
2023-10-09 15:21:29 +0200

    finished script

--- js/script.js
+++ js/script.js
@@ -6,9 +6,27 @@
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 // Questo richiederà un minimo di ricerca.
 
-let distance = prompt('Inserire distanza da percorrere: (in chilometri)');
+let distance = parseInt(prompt('Inserire distanza da percorrere: (in chilometri)'));
 
-let age = prompt('Inserire età del passeggero:');
+let age = parseInt(prompt('Inserire età del passeggero:'));
 
-let ticketPrice = (distance * 0.21);
+const kmPrice = 0.21;
 
+let total = (distance * kmPrice);
+
+let finalPrice;
+
+if (isNaN(distance) || (isNaN(age))) {
+    location.reload();
+}
+
+if (age < 18){
+    finalPrice = (total - (total * 20 / 100)).toFixed(2) + " €";
+    document.getElementById("prezzo").innerHTML = "Il prezzo finale è " + finalPrice;
+} else if (65 <= age) {
+    finalPrice = (total - (total * 40 / 100)).toFixed(2) + " €";
+    document.getElementById("prezzo").innerHTML = "Il prezzo finale è " + finalPrice;
+} else {
+    total = (total.toFixed(2) + " €");
+    document.getElementById("prezzo").innerHTML = "Il prezzo finale è " + total;
+} 
